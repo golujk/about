54405c3f4803b376ceadbaff7be8f6b69ae1cac1
golujk <108279138+golujk@users.noreply.github.com>
2022-06-28 15:22:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+  $(".a").click(function(){
+  $(".clip-top-1").css("background","#FFAB00");
+  $(".clip-top-1").css("transition","0.6s")
+  $(".btn").css("background","#FFAB00");
+  $(".btn").css("transition","0.6s")
+  $(".clip-bottom").css("background","#FFAB00");
+  });
+  
+  $(".b").click(function(){
+  $(".clip-top-1").css("background","#36B37E");
+  $(".clip-top-1").css("transition","0.6s")
+  $(".btn").css("background","#36B37E");
+  $(".btn").css("transition","0.6s")
+  $(".clip-bottom").css("background","#36B37E");
+  });
+  
+  $(".c").click(function(){
+  $(".clip-top-1").css("background","#5E6C84");
+  $(".clip-top-1").css("transition","0.6s")
+  $(".btn").css("background","#5E6C84");
+  $(".btn").css("transition","0.6s")
+  $(".clip-bottom").css("background","#5E6C84");
+  });
+  
+  $(".d").click(function(){
+  $(".clip-top-1").css("background","#0BC5EA");
+  $(".clip-top-1").css("transition","0.6s")
+  $(".btn").css("background","#0BC5EA");
+  $(".btn").css("transition","0.6s")
+  $(".clip-bottom").css("background","#0BC5EA");
+  });
+  
+  $(".e").click(function(){
+  $(".clip-top-1").css("background","#8954f2");
+  $(".clip-top-1").css("transition","0.6s")
+  $(".btn").css("background","#8954f2");
+  $(".btn").css("transition","0.6s")
+  $(".clip-bottom").css("background","#8954f2");
+  });
+  
+  $(".f").click(function(){
+  $(".clip-top-1").css("background","#FF5630");
+  $(".clip-top-1").css("transition","0.6s")
+  $(".btn").css("background","#FF5630");
+  $(".btn").css("transition","0.6s")
+  $(".clip-bottom").css("background","#FF5630");
+  });
+  
+});
